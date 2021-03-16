9e1d07604ad308c0391abc13c56176e43e05bd2a
mark <mark@marks-MacBook-Pro.local>
2021-03-16 01:35:08 -0700

    Added UV Index

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// var chooseCity="Chicago"
+// Define Variables
 var transPick =""
 var cityPicked=[];
 var storedCity=[];
@@ -14,7 +14,10 @@ var iconBegin = "http://openweathermap.org/img/wn/"
 var iconEnd = "@2x.png"
 var step = 0;
 var linkBuild =""
-
+var uvLink="https://api.openweathermap.org/data/2.5/onecall?lat="
+var uvMid="&lon="
+var uvlinkBuild=""
+var uvDex = 0;
 dataURL ="https://api.openweathermap.org/data/2.5/weather?q=";
 trailURL="&appid=bd61ef1d733089162a77b7578f04dddc"
 comboURL=""
@@ -28,12 +31,12 @@ let windSpeed = 0;
 
 // conversion code Courtesy brettvaida GitHub Gist.
 let tempConvert =function(){
-console.log("KT " + kelvinTemp)
+// console.log("KT " + kelvinTemp)
 const celsius = kelvinTemp - 273;
 let fahrenheit = Math.floor(celsius * (9/5) + 32);
 
 // Displaying the temperature using string interpolation
-console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`)
+// console.log(`The temperature is ${fahrenheit} degrees fahrenheit.`)
 return fahrenheit;
 }
 
@@ -49,15 +52,49 @@ fetch(finalURL).then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    // console.log(data);
     kelvinTemp = data.main.temp;
     localHumid = data.main.humidity;
     windSpeed = data.wind.speed;
-    console.log("temp in Kelvin " + kelvinTemp);
-    console.log("Humidity: " + localHumid);
-    console.log("Wind Speed: " + windSpeed);
+    lati = data.coord.lat;
+    long = data.coord.lon;
+    ucomboURL = uvLink.concat(lati);
+    mcomboURL = ucomboURL.concat(uvMid)
+    linkBuild = mcomboURL.concat(long);
+    uvlinkBuild = linkBuild.concat(trailURL)
+    console.log(uvlinkBuild)
+    fetch(uvlinkBuild).then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        uvDex = data.current.uvi;
+        console.log("uvIndex: " + uvDex);
+    // console.log("temp in Kelvin " + kelvinTemp);
+    // console.log("Humidity: " + localHumid);
+    // console.log("Wind Speed: " + windSpeed);
 
   });
+
+//   let ucomboURL = uvLink.concat(lati);
+//   let mcomboURL = ucomboURL.concat(uvMid)
+//   let linkBuild = mcomboURL.concat(long);
+//   let uvlinkBuild =linkBuild.concat(trailURL)
+  
+// document.getElementById("cityStats").innerHTML = chooseCity;
+fetch(uvlinkBuild).then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    
+    
+    // console.log("Humidity: " + localHumid);
+    // console.log("Wind Speed: " + windSpeed);
+
+  });
+  
+  
 
 
 let fcomboURL = fiveURL.concat(chooseCity);
@@ -67,21 +104,21 @@ let secondURL = fcomboURL.concat(trailURL);
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    // console.log(data);
     
     for (i=0; i<5; i++){
-        console.log ("iteration: "+ i)
+        // console.log ("iteration: "+ i)
         fiveDayIcon[i] = data.list[step].weather[0].icon;
-        console.log("Icon Number "+ fiveDayIcon[i]);
+        // console.log("Icon Number "+ fiveDayIcon[i]);
         fiveDayTemp[i] = data.list[step].main.temp;
-        console.log("temp is " + fiveDayTemp[i]);
+        // console.log("temp is " + fiveDayTemp[i]);
         fiveDayHumid[i] = data.list[step].main.humidity;
-        console.log("humidity is " + fiveDayHumid[i]);
-        console.log("step is " + step)
+        // console.log("humidity is " + fiveDayHumid[i]);
+        // console.log("step is " + step)
         step += 8;
         linkBuild=iconBegin.concat(fiveDayIcon[i]);
         iconLink[i]=linkBuild.concat(iconEnd)
-        console.log("icon link: " + iconLink[i])
+        // console.log("icon link: " + iconLink[i])
 
 //let fahrenheitToKelvin = tempConvert(data.main.temp)
 
@@ -89,14 +126,21 @@ let secondURL = fcomboURL.concat(trailURL);
     tempConvert();
     dateCalc();
   });
-
+//  Save Chosen City to Local Storage
 console.log(chooseCity);
 console.log(finalURL);
-
 localStorage.setItem("storedCity[cityCount]", chooseCity);
 transPick = localStorage.getItem("storedCity[cityCount]");
-console.log("retained city:" + transPick)
+console.log("retained city:" + storedCity[cityCount])
 cityPicked[cityCount] = transPick;
+cityCount++;
+console.log(cityCount);
+console.log("city Picked is: " + transPick);
+
+if (cityCount > 9){cityCount = 0}
+
+
+
 document.getElementById("cityZro").innerHTML = cityPicked[0];
 document.getElementById("cityOne").innerHTML = cityPicked[1];
 document.getElementById("cityTwo").innerHTML = cityPicked[2];
@@ -107,14 +151,8 @@ document.getElementById("citySix").innerHTML = cityPicked[6];
 document.getElementById("citySev").innerHTML = cityPicked[7];
 document.getElementById("cityEig").innerHTML = cityPicked[8];
 document.getElementById("cityNin").innerHTML = cityPicked[9];
-cityCount++;
-console.log(cityCount);
-console.log("city Picked is: " + transPick);
-
-if (cityCount > 9){cityCount = 0}
-
 });
-
+});
 
 
 
@@ -131,6 +169,17 @@ document.getElementById("cityName").innerHTML = chooseCity +"  "+ timeNow;
 document.getElementById("cityTemp").innerHTML = "Temperature: " + fahrenheit + " Degrees";
 document.getElementById("cityHumid").innerHTML = "Humidity: " + localHumid + "%";
 document.getElementById("cityWind").innerHTML = "Wind Speed: " + windSpeed + " MPH";
+document.getElementById("uvIndex").innerHTML = "UV Index: " + uvDex;
+
+if (uvDex > 3 && uvDex <6){
+    document.getElementById("uvIndex").style.backgroundColor = "yellow"
+} else if (uvDex > 5 && uvDex < 8){
+    document.getElementById("uvIndex").style.backgroundColor = "orange"  
+} else if (uvDex > 7){
+    document.getElementById("uvIndex").style.backgroundColor = "red"  
+} else {
+    document.getElementById("uvIndex").style.backgroundColor = "white" 
+}
 const day1 = moment(todayDate, "MM DD YY").add(1, 'days').format("MM DD YY");
 const day2 = moment(todayDate, "MM DD YY").add(2, 'days').format("MM DD YY");
 const day3 = moment(todayDate, "MM DD YY").add(3, 'days').format("MM DD YY");
